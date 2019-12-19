f00f98bf08baba08fd5bab79bd3c30941c153766
jamaral14 <amaralmm14@hotmail.com>
2019-12-19 10:07:31 +0000

    added handover.html

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 import os
-from flask import Flask
+import env
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
+
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'handover_manager'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', "Env value not loaded")
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_handover')
+def get_handover():
+    return render_template("handover.html", handover=mongo.db.handover.find())
 
 
 if __name__ == '__main__':

8dad6711b14b782403436875449daf37fb3bf6b1
jamaral14 <amaralmm14@hotmail.com>
2019-12-20 17:00:11 +0000

    added edithandover.html

--- app.py
+++ app.py
@@ -27,7 +27,14 @@ def get_addhandover():
 def insert_handover():
     handover = mongo.db.handover
     handover.insert_one(request.form.to_dict())
-    return redirect(url_for('get_handover'))    
+    return redirect(url_for('get_handover'))  
+
+@app.route('/edit_handover/<handover_id>')
+def edit_handover(handover_id):
+    the_handover =  mongo.db.handover.find_one({"_id": ObjectId(handover_id)})
+    all_sections =  mongo.db.sections.find()
+    return render_template('edithandover.html', handover=the_handover,
+                           sections=all_sections)      
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP', '0.0.0.0'),
